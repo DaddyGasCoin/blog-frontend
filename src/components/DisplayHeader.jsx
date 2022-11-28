407dfd48c089c9bc82dd8e70a7b4130af80b3f20
Michael Jones <iammichaeljon27@gmail.com>
2022-11-28 12:49:08 +0400

    Add logout button

--- src/components/DisplayHeader.jsx
+++ src/components/DisplayHeader.jsx
@@ -1,10 +1,34 @@
-import { Button } from 'antd';
+import { Button, Dropdown } from 'antd';
 import { useEffect, useState } from 'react';
 import { Outlet } from "react-router-dom";
 
 const DisplayHeader = () => {
 
+
    const [user, setUser] = useState(false)
+
+   //Clear token info from session/local stoarge to logout
+   const logout = () => {
+      const token = sessionStorage.getItem('token_info')
+      if (token) {
+         sessionStorage.removeItem('token_info')
+      }
+      else {
+         localStorage.removeItem('token_info')
+      }
+      document.location.href = '/'
+
+   }
+   const items = [
+      {
+         key: '1',
+         label: (
+            <a onClick={logout} >
+               Logout
+            </a>
+         ),
+      }
+   ];
 
    useEffect(() => {
       //TOKEN contains usersname and JWT for authentication
@@ -32,9 +56,20 @@ const DisplayHeader = () => {
       <>
          <div className="flex flex-row-reverse py-1 px-1">
             <div className="flex flex-row-reverse py-1 px-1">
-               <Button type="text" size='large' href='/login'>
-                  {user ? user : 'Login'}
-               </Button>
+               {user ?
+                  <Dropdown
+                     menu={{
+                        items,
+                     }}
+                     placement='bottomLeft'
+                  >
+                     <Button type="text" size='large'>{user}</Button>
+                  </Dropdown>
+                  :
+                  <Button type="text" size='large' href='/login'>
+                     Login
+                  </Button>
+               }
                <Button type="text" size='large' href='/signup'>Sign-Up</Button>
             </div>
          </div>
